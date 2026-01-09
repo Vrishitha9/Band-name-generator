f123d97d6fa392c4b8e1b81d250ad4f58b731e97
Vrishitha9 <grishithanikhileshreddy@gmail.com>
2026-01-09 18:35:09 +0530

    Add files via upload

---
+++ Band name generator day 1.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name generator")
+city =input("In which city did you grow up?\n")
+letter=input("What is your favorite letter?\n")
+print("Your band name is:" + city + " " + letter)
